b5ca3c8380253d6ffe7be4bd97ccfa60c038dc79
Walter <walterdcarrizo.19@gmail.com>
2023-07-23 20:58:06 -0300

    Se genera una precargaa de data y boton activ al seleccionar categoría

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -12,7 +12,25 @@ export const useLabels = () => {
         ['labels'],
         getLabels,
         {
-            staleTime: 1000 * 60 * 60
+            staleTime: 1000 * 60 * 60,
+            // initialData
+            placeholderData: [
+                {
+                    id: 739761016,
+                    node_id: "MDU6TGFiZWw3Mzk3NjEwMTY=",
+                    url: "https://api.github.com/repos/facebook/react/labels/Component:%20Reconciler",
+                    name: "Component: Reconciler",
+                    color: "f9a798",
+                    default: false
+                },
+                {   id: 52079258,
+                    node_id: "MDU6TGFiZWw1MjA3OTI1OA==",
+                    url: "https://api.github.com/repos/facebook/react/labels/Difficulty:%20starter",
+                    name: "Difficulty: starter",
+                    color: "94ce52",
+                    default: false
+                }
+            ]
         }
       )
     
