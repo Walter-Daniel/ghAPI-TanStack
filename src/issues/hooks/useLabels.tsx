629a2df43665ec3f7569dad10f6c77c954583d96
Walter <walterdcarrizo.19@gmail.com>
2023-07-23 19:00:01 -0300

    Instalación de TanStack, providers, client, generación estado asincrónico

---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+
+const getLabels = async():Promise<Label[]> => {
+    const { data } = await githubApi.get<Label[]>('/labels')
+    return data
+}
+
+export const useLabels = () => {
+    const labelQuery = useQuery(
+        ['labels'],
+        getLabels,
+        {
+            staleTime: 1000 * 60 * 60
+        }
+      )
+    
+    return labelQuery
+}
